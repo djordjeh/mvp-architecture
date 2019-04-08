b9e47d3cb21f69c0aaf1a0bbbc4cba25ba7a3606
Dorde Hrnjez <d.hrnjez@holidaypirates.com>
2019-04-08 15:10:13 +0200

    CustomMatcher moved to sharedTest folder

--- app/src/sharedTest/java/djordjeh/architecture/mvp/util/CustomMatchers.java
+++ app/src/sharedTest/java/djordjeh/architecture/mvp/util/CustomMatchers.java
@@ -1,18 +1,20 @@
 package djordjeh.architecture.mvp.util;
 
+import android.os.IBinder;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.test.espresso.Root;
 import androidx.test.espresso.matcher.BoundedMatcher;
 
 public class CustomMatchers {
@@ -62,6 +64,28 @@ public class CustomMatchers {
                 } else {
                     return item.isErrorEnabled() && errorMessage.equals(item.getError());
                 }
+            }
+        };
+    }
+
+    public static Matcher<Root> isToast() {
+        return new TypeSafeMatcher<Root>() {
+            @Override
+            protected boolean matchesSafely(Root root) {
+                int type = root.getWindowLayoutParams().get().type;
+                if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+                    IBinder windowToken = root.getDecorView().getWindowToken();
+                    IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                    // windowToken == appToken means this window isn't contained by any other windows.
+                    // if it was a window for an activity, it would have TYPE_BASE_APPLICATION.
+                    return windowToken == appToken;
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is Toast message");
             }
         };
     }
